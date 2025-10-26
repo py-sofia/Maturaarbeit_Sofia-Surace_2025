193cb6e8288a0bdfc1518a85944d1a5b7923520c
Sofia Surace <sofia.surace.a@gmail.com>
2025-10-26 10:10:32 +0100

    Add degree distribution analysis

--- code/utils/pick_beta1.R
+++ code/utils/pick_beta1.R
@@ -16,9 +16,45 @@ beta1 <- 3 # hand-picked
 
 head(sft$fitIndices)
 
+png("results/scale_free_topology_model.png", width = 1200, height = 700, res = 150)
 plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
      xlab="Soft Threshold (power)", ylab="Scale Free Topology Model Fit, signed R^2",
-     type="n", main="Scale independence")
+     type="n")
 text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
      labels=powers, cex=0.9, col="red")
 abline(h=0.85, col="red")
+dev.off()
+
+
+
+################################################################################
+
+
+degTest <- degree(netTest, mode = "all")
+degControl <- degree(netControl, mode = "all")
+
+freqControl <- as.data.frame(table(as.numeric(degControl)))
+freqControl$degree <- as.numeric(as.character(freq$Var1))
+
+freqTest <- as.data.frame(table(as.numeric(degTest)))
+freqTest$degree <- as.numeric(as.character(freqTest$Var1))
+
+png("results/degree_distribution.png", width = 1200, height = 500, res = 150)
+par(mfrow = c(1, 2))
+
+plot(
+  log1p(freqControl$degree), log1p(freqControl$Freq),
+  pch = 19, cex = 0.7, col = rgb(0.1, 0.6, 0.1, 0.4),      
+  main = "netControl",
+  xlab = "log1p(Degree)", ylab = "log1p(Frequency)", las = 1, bty = "l")
+
+plot(
+  log1p(freqTest$degree), log1p(freqTest$Freq),
+  pch = 19, cex = 0.7, col = rgb(0.1, 0.6, 0.1, 0.4),      
+  main = "netTest",
+  xlab = "log1p(Degree)", ylab = "log1p(Frequency)", las = 1, bty = "l")
+
+dev.off()
+
+
+
